8a61a9c2980a1d004903c287809ebc4cf0598443
Sparsh <sparsh.chadha46@gmail.com>
2023-01-06 23:13:12 +0530

    title changed

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-        title: "Mera app hai ji",
+        title: "MyApp",
         home: HomePage(),
     );
   }
